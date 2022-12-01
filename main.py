5a36d3c2e29908b32a338269371ea0a9490d68f4
Wafi <wafic49@gmail.com>
2022-12-01 14:11:48 +0000

    updated app

--- main.py
+++ main.py
@@ -3,16 +3,40 @@ from flask import (Flask,
                    url_for,
                    send_from_directory)
 
-from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_uploads import UploadSet, configure_uploads
 
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 import pyttsx3
+import pickle
+from detecto.core import Model
+from detecto import utils
+
+from pathlib import Path
+import playsound
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "consumerlending"
 app.config["UPLOADED_PHOTOS_DEST"] = 'uploads'
+
+model = Model.load(r"C:\dev\projects\repos\flask_app\model\recycle.pth", ["recycle"])
+
+# def serve_model(image):
+#     image = utils.read_image("images/IMG_0094.JPG")
+#     model = pickle.load()
+#
+#     predictions = model.predict(image)
+#     # predictions format: (labels, boxes, scores)
+#     labels, boxes, scores = predictions
+#     # [‘alien’, ‘bat’, ‘bat’]
+#     print(labels)
+#     print(boxes)
+#     # tensor([0.9952, 0.9837, 0.5153])
+#     print(scores)
+#     visualize.show_labeled_image(image, boxes, labels)
+#     return True
 
 photos = UploadSet('photos', tuple("png jpg jpeg mp4".split()))
 configure_uploads(app, photos)
@@ -27,25 +51,15 @@ class UploadForm(FlaskForm):
     submit = SubmitField("Test item")
 
 
-def text_to_speech(text):
+def play_soundfile(soundfile):
     """
     Function to convert text to speech
     :param text: text
     :param gender: gender
     :return: None
     """
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 125)
-    engine.setProperty('volume', 0.8)
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)
-    engine.say(text)
-    engine.runAndWait()
+    playsound.playsound(soundfile)
 
-def serve_model():
-    #
-
-    return True
 
 @app.route('/uploads/<filename>')
 def get_file(filename):
@@ -57,27 +71,44 @@ def get_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
     form = UploadForm()
-
+    recyclable = False
+    frames_p = Path('./uploads/frames')
+    upload_p = Path('./uploads')
+    print("BEFORE IF STATEMENT SUBMIT")
     if form.validate_on_submit():
         filename = photos.save(form.photo.data)
         file_url = url_for("get_file", filename=filename)
 
-        #Test out the model here
-        model_result = False
+
+        video = r"C:\dev\projects\repos\flask_app\uploads\{}".format(filename)
+        utils.split_video(video,  step_size=5, output_folder=r"C:\dev\projects\repos\flask_app\uploads\frames")
+        print("please work")
+
+        try:
+            for frame in list(frames_p.glob("**/frame*")):
+                image = utils.read_image(str(frame))
+                labels, boxes, scores = model.predict(image)
+                print(f"in loop: {scores}")
+                if any(scores > 0.6):
+                    recyclable = True
+                    break
+        finally:
+            for frame in list(frames_p.glob("**/frame*")):
+                os.remove(frame)
+            for vid in list(upload_p.glob("**/*.mp4")):
+                os.remove(vid)
+
 
         #Change file_url to where the image detected is
 
-
-        if model_result:
-            text = 'The item is recyclable'
-            text_to_speech(text)
+        if recyclable:
+            play_soundfile(r"C:\dev\projects\repos\flask_app\sounds\yes_recycle_david.mp3")
             return render_template('recyclable.html', form2=form, file_url=file_url)
         else:
-            text = 'The item is not recyclable'
-            text_to_speech(text)
+            play_soundfile(r"C:\dev\projects\repos\flask_app\sounds\not_recyclable_david.mp3")
             return render_template('nonrecyclable.html', form2=form, file_url=file_url)
     else:
-        file_url=None
+        file_url = None
 
     return render_template('home.html', form2=form, file_url=file_url)
 
